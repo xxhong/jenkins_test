827e0ce0853c3ae7990150dd1833792fc9809785
xuxihong <xuxihong@minijoy.work>
2020-12-28 22:09:58 +0800

    测试一下兄弟

--- post.py
+++ post.py
@@ -8,11 +8,12 @@ def execCmd(cmd):
     return text
 txt1 = execCmd('git rev-parse HEAD')
 print("txt1:"+txt1)
-gitTxt = 'git log --pretty=format:"%s" '+txt1+ " -1"
-print(gitTxt)
+gitTxt = str("git log --pretty=format:%s") +str(txt1)+ str(" -1")
+print("gitTxt:[[[["+gitTxt)
+print("str():"+str("%s"))
 txt2 = execCmd(gitTxt)
 
-print("txt2:  "+txt2)
+print("最后文本:  [[[["+txt2+"】】】】")
 headers = {'Content-Type': 'application/json'}
 jsonData ={"msg_type": "text","content":{"text":txt2}};
 feiShuUrl='https://open.feishu.cn/open-apis/bot/v2/hook/3f368105-c7e9-474c-9c94-f3d7b4ea7ef8'
